51e403c282a069f242fd46a9b3a2e23cbe520a30
Samira Boudjemai <sboudjemai@xpanxion.com>
2023-05-24 17:20:01 -0400

    Updates ex7

--- src/Student15/Ex7/Main.js
+++ src/Student15/Ex7/Main.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Main.css'
 import { CarCounter } from './CarCounter'
+
 export const Main = () => {
+
   return (
+
     <div>
       <CarCounter />
     </div>
+    
   )
 }
